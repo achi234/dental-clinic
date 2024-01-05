fd40fbc0dd7162d522e6926c74ae2b121a938cab
BNKieuNhi <93073321+BNKieuNhi@users.noreply.github.com>
2024-01-05 22:51:23 +0700

    Create change_profile.php

---
+++ Source/Website/DentalClinic/Controller/DentistController/change_profile.php
@@ -0,0 +1,63 @@
+<?php
+    include('../functions.php');
+    include("../../config/config.php");
+    session_start();
+    
+    if(isset($_POST['btn-changeProfile']))
+    {
+        if(empty($_POST['fullname']) || empty($_POST['user_phone'])|| empty($_POST['user_gender'])|| empty($_POST['user_address']))
+        {
+            //echo 'Here';
+            redirect('../../MainUI/DentistUI/change_profile.php', 'All fields are required.', '');
+            exit(0);
+        }
+        else
+        {
+            $id = $_SESSION['auth_user']['id'];
+            $username =  $_SESSION['auth_user']['username'];
+            $fullname = $_POST['fullname'];
+            $phone = $_POST['user_phone'];
+            $gender = $_POST['user_gender'];
+            $address = $_POST['user_address'];
+            $password = $_POST['new_password'];
+
+
+            if(empty($password))
+            {
+                $info = getbyKeyValue('ACCOUNT', 'Username',$username);
+                $password = $info['data']['Pass_word'];
+            }
+
+            $dataUser = [
+                'Fullname'    => $fullname,
+                'Gender'      => $gender,
+                'CurrAddress' => $address,
+                'PhoneNumber' => $phone,
+            ];
+
+            $updateUser = updatebyKeyValue('USER_DENTAL', 'ID_User', $id, $dataUser);
+            // echo $username;
+            // echo '<br>';
+            // echo $fullname;
+            // echo '<br>';
+            // echo $phone;
+            // echo '<br>';
+            // echo $gender;
+            // echo '<br>';
+            // echo $address;
+            // echo '<br>';
+            // echo $password;
+            // echo '<br>';
+
+            if($updateUser['status'])
+            {
+                redirect('../../MainUI/DentistUI/change_profile.php', '', "You've update your information successfully!");
+            }
+            else
+            {
+                redirect('../../MainUI/DentistUI/change_profile.php', 'Something went wrong! Please enter again...', "");
+            }
+
+        }
+    }
+?>

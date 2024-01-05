72376d219f2f86ef375e4fab560b7845d9411361
BNKieuNhi <93073321+BNKieuNhi@users.noreply.github.com>
2024-01-05 22:52:09 +0700

    Create delete_chooseTooth.php

---
+++ Source/Website/DentalClinic/Controller/StaffController/delete_chooseTooth.php
@@ -0,0 +1,33 @@
+<?php
+
+    include('../functions.php');
+    include("../../config/config.php");
+
+    session_start();
+
+    global $conn;
+
+    $param = checkParam('id');
+
+    $tooth_id = checkParam('tooth_id');
+    $surface_id = checkParam('surface_id');
+
+    echo $param;
+    echo $tooth_id;
+    echo $surface_id;
+
+
+    $query = "DELETE FROM CHOOSE_TOOTH WHERE ID_Select= '$param' AND ID_TOOTH='$tooth_id' AND ID_Surface= '$surface_id' ";
+   
+    $result = sqlsrv_query($conn, $query);
+
+    if($result)
+    {
+        redirect('../../MainUI/StaffUI/update_treatmentplans.php?id='.$param, '', 'Delete tooth choice successfully');
+    }
+    else
+    {
+        redirect('../../MainUI/StaffUI/update_treatmentplans.php?id='.$param, 'Cannot delete tooth choice. Please try again!', '');
+    }
+
+?>

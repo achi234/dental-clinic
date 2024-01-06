c13564e5494171eeecf2ed59cdca2cb99119bab0
BNKieuNhi <93073321+BNKieuNhi@users.noreply.github.com>
2024-01-06 14:36:06 +0700

    Update Trigger.sql

--- Source/Database/Trigers/Trigger.sql
+++ Source/Database/Trigers/Trigger.sql
@@ -1,13 +1,18 @@
 ﻿USE QLPHONGKHAMNHAKHOA 
 GO
 
+--drop trigger tg_kiemtra_CUOCHEN
+--drop trigger tg_add_KeDonThuoc_KEDON
+--drop trigger tg_kiemtra_HanSuDung_KEDON
+--drop trigger tg_add_ThanhTien_KEDON
+--drop trigger tg_add_TongTienThuoc_HOSO
 -- Nội dung: Lịch hẹn mà khách hàng đặt không được trùng với lịch hẹn của bác sĩ.
 create trigger tg_kiemtra_CUOCHEN
 on CUOCHEN
-for insert 
+for insert, update
 as
 begin
-	if exists (select 1
+	/*if exists (select 1
 				from CUOCHEN c
 				inner join NHASI n on c.SDT_NS = n.SDT_NS
 				where c.ID_CuocHen in (select ID_CuocHen from inserted)
@@ -18,15 +23,24 @@ begin
 								and h.ID_HoSo <> (select ID_HoSo from inserted)
 								)
 				)
+	*/
+	if exists (select 1
+				from CUOCHEN c1, CUOCHEN c2
+				where c1.ID_CuocHen != c2.ID_CuocHen
+				and c1.Ngay = c2.Ngay
+				and c1.Gio = c2.Gio
+				and c1.SDT_NS = c2. SDT_NS
+				)
 	begin
 		raiserror('Bac si khong ranh trong thoi gian nay', 16, 1)
 		rollback transaction
 	end
 end
+go
 -- Nội dung: Số lượng thuốc kê đơn phải nhỏ hơn hoặc bằng số lượng tồn kho. 
 create trigger tg_add_KeDonThuoc_KEDON
 on KEDON
-for insert
+for insert, update
 as
 begin
 	if exists (select 1
@@ -40,10 +54,11 @@ begin
 		rollback transaction
 	end
 end
+go
 -- Nội dung: Thuốc kê đơn không được quá hạn sử dụng
 create trigger tg_kiemtra_HanSuDung_KEDON
 on KEDON
-for insert
+for insert, update
 as
 begin
 	if exists (select 1
@@ -57,12 +72,12 @@ begin
 		rollback transaction
 	end
 end
-
+go
 -- Nội dung: Thành tiền của mỗi loại thuốc được kê bằng đơn giá * số lượng
 -- ThanhTien(KEDON) = SoLuong * DonGia
 create trigger tg_add_ThanhTien_KEDON
 on KEDON
-after insert
+after insert, update
 as
 begin
 	declare @id_hoso char(10)
@@ -89,15 +104,23 @@ begin
 	update KEDON
 	set THANHTIEN = @thanh_tien
 	where ID_HOSO = @id_hoso and ID_THUOC = @id_thuoc
+
+	declare @tong_tien_thuoc int
+	select @tong_tien_thuoc = ISNULL(SUM(ThanhTien), 0)
+	from KEDON where ID_HoSo = @id_hoso
+
+	update HOSOKHACHHANG
+	set TongTienThuoc = @tong_tien_thuoc
+	where ID_HoSo = @id_hoso
+
 end
+go
 -- Nội dung: Tổng chi phí thanh toán cho 1 hồ sơ bệnh nhân phải bằng 
 -- tổng chi phí dịch vụ, phí khám bệnh và thuốc được kê. 
 -- TongTien(HSKH) = PhiDV + PhiKham + TongTienThuoc
-drop trigger tg_add_TongTienThuoc_HOSO
-
 create trigger tg_add_TongTienThuoc_HOSO
 on HOSOKHACHHANG
-after insert
+after insert, update
 as
 begin
 	declare @id_hoso char(10)
@@ -113,8 +136,8 @@ begin
 	from inserted
 
 	-- TongTienThuoc(HSKH) = SUM(ThanhTien(KEDON))
-	select @tong_tien_thuoc = isnull(sum(THANHTIEN), 0)
-	from KEDON where ID_HOSO = @id_hoso
+	select @tong_tien_thuoc = ISNULL(SUM(ThanhTien), 0)
+	from KEDON where ID_HoSo = @id_hoso
 
 	update HOSOKHACHHANG
 	set TongTienThuoc = @tong_tien_thuoc

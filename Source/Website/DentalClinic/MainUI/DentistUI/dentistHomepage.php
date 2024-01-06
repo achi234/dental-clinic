fff96f79c0b2b3cb8fc7800714c972c454f30655
anhchi7103 <nlachi21@clc.fitus.edu.vn>
2024-01-06 19:47:22 +0700

    update

--- Source/Website/DentalClinic/MainUI/DentistUI/dentistHomepage.php
+++
@@ -1,6 +0,0 @@
-<?php
-    session_start();
-    echo "Role is {$_SESSION['auth_user']['role']} ";
-?>
-
-<h6>This is Dentist Homepage</h6>
--- Source/Website/DentalClinic/MainUI/DentistUI/partials/_footer.php
+++
@@ -1,6 +0,0 @@
-<div class="footer">
-    <div class="container footer__content">
-        <p class="footer__content-develop">December, 2023 - AdvDB - HCMUS</p>
-        <a href="" class="footer__content-brand-name">Smile</a>
-    </div>
-</div>
